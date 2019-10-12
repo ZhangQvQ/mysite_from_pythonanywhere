bfcc2665cb0f89139184507f28a4084f5f2f0541
Zhangqi <m13281879546@gmail.com>
2019-10-12 17:58:59 +0800

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('avgle/', inlcude('avgle.urls')),
+    path('avgle/', include('avgle.urls')),
     ]
