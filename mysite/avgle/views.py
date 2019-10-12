ec7c331ba7e1e3d0fdbe662d6274a3b539a355bd
Zhangqi <m13281879546@gmail.com>
2019-10-12 17:56:27 +0800

    add avgle/index.html

--- mysite/avgle/views.py
+++ mysite/avgle/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
+def hello(request):
     return HttpResponse("Hello world!")
 
+def index(request):
+    context_dict = {'text': "Hello World!",}
+    response = render(request, 'avgle/index.html', context=context_dict)
+    return response
