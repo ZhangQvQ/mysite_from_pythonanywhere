0aebb8a268d2330afb1c3caa6e5e4db6ab8c21a6
zhangqi_pythonanywhere <m13281879546@gmail.com>
2019-10-14 11:48:11 +0000

    没有好看的模板，干

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
+def hello(request):
     return HttpResponse("Hello World!")
 
+def index(request):
+    context = {'title': 'welcome', 'text': 'Hello!'}
+    return render(request, 'blog/index.html', context)
