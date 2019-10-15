b5e2cfb46a540f22a6d5e136c5d7119617593cf2
Zhangqi <m13281879546@gmail.com>
2019-10-15 13:52:52 +0800

    fix

--- mysite/comments/forms.py
+++ mysite/comments/forms.py
@@ -3,5 +3,5 @@ from comments.models import Comment
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = CommentForm
+        model = Comment
         fields = ['name', 'email', 'url', 'text']
